1ecfa7d6d7c8f38bbaf3d325bbfc0df67e1015c4
John DeSilvio <john.desilvio@yahoo.com>
2017-03-10 22:22:51 -0500

    Created Users model

---
+++ web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule Blaces.User do
+  use Blaces.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :username, :string
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :username, :password_hash])
+    |> validate_required([:email, :username, :password_hash])
+    |> unique_constraint([:user_email_index, :user_username_index])
+  end
+end
