784db3f2a9b75c7ee76a0d89e0cf1b7e6902a446
John DeSilvio <john.desilvio@yahoo.com>
2017-03-12 21:28:12 -0400

    Added routes for authenticated users; fixed bug in yelp controller and template

--- web/router.ex
+++ web/router.ex
@@ -13,11 +13,18 @@ defmodule Blaces.Router do
     pipe_through [:browser, :with_session]
 
     get "/", PageController, :index
-    get "/yelp", YelpController, :index
-    post "/yelp", YelpController, :index
-
-    resources "/users", UserController, only: [:show, :new, :create]
+    resources "/users", UserController, only: [:new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+
+    # Authenticated user
+    scope "/"  do
+      pipe_through [:login_required]
+
+      resources "/users", UserController, only: [:show]
+
+      get "/yelp", YelpController, :index
+      post "/yelp", YelpController, :index
+    end
   end
 
   pipeline :api do
@@ -35,6 +42,11 @@ defmodule Blaces.Router do
     plug Guardian.Plug.VerifySession
     plug Guardian.Plug.LoadResource
     plug Blaces.CurrentUser
+  end
+
+  pipeline :login_required do
+    plug Guardian.Plug.EnsureAuthenticated,
+         handler: Blaces.GuardianErrorHandler
   end
 
 end
