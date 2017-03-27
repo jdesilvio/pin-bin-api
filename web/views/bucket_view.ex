a4ceb622985086e05ca42d2693100005baefd275
John DeSilvio <john.desilvio@yahoo.com>
2017-03-26 20:47:53 -0400

    Added views for rendering JSON

--- web/views/bucket_view.ex
+++ web/views/bucket_view.ex
@@ -1,4 +1,15 @@
 defmodule Blaces.BucketView do
   use Blaces.Web, :view
 
+  def render("index.json", %{buckets: buckets}) do
+    %{data: render_many(buckets, Blaces.BucketView, "bucket.json")}
+  end
+
+  def render("bucket.json", %{bucket: bucket}) do
+    %{id: bucket.id,
+      name: bucket.name,
+      short_name: bucket.short_name,
+      is_public: bucket.is_public}
+  end
+
 end
