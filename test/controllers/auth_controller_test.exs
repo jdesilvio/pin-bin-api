826a0a88b5c9f6d255a5348ae86f94636b7b9373
John DeSilvio <john.desilvio@yahoo.com>
2017-04-14 21:31:31 -0400

    Added tests

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -1,11 +1,13 @@
 defmodule Blaces.AuthControllerTest do
   use Blaces.ConnCase
 
+  import Blaces.Factory
+
   alias Blaces.Repo
   alias Blaces.User
 
   setup do
-    user = %User{id: 1, username: "Moe", email: "moe@stooges.com"}
+    user = user_factory()
     {:ok, jwt, full_claims} = Guardian.encode_and_sign(user)
     {:ok, %{user: user, jwt: jwt, claims: full_claims}}
   end
@@ -14,6 +16,7 @@ defmodule Blaces.AuthControllerTest do
     conn = build_conn()
       |> put_req_header("authorization", "Bearer #{jwt}")
       |> get("/api/v1/auth")
-    IO.inspect conn
+
+    assert conn == ""
   end
 end
