cd853da62b546af3c43caa455e092e257a19e868
John DeSilvio <john.desilvio@yahoo.com>
2018-08-10 19:28:49 -0400

    Reference new namespace in configs

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :blaces, Blaces.Endpoint,
+config :blaces, BlacesWeb.Endpoint,
   http: [port: System.get_env("PORT") || 4001],
   server: false
 
