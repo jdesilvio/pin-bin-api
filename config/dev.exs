81d1a391c5759b2da751d2d4045a3562837df97c
John DeSilvio <john.desilvio@yahoo.com>
2018-08-20 21:25:34 -0400

    Update /config Blaces to PinBin

--- config/dev.exs
+++ config/dev.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
-config :blaces, BlacesWeb.Endpoint,
+config :blaces, PinBinWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   debug_errors: true,
   code_reloader: true,
@@ -15,7 +15,7 @@ config :blaces, BlacesWeb.Endpoint,
                     cd: Path.expand("../assets", __DIR__)]]
 
 # Watch static and templates for browser reloading.
-config :blaces, BlacesWeb.Endpoint,
+config :blaces, PinBinWeb.Endpoint,
   live_reload: [
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
@@ -33,7 +33,7 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :blaces, Blaces.Repo,
+config :blaces, PinBin.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :blaces, BlacesWeb.Endpoint,
+config :blaces, PinBinWeb.Endpoint,
   http: [port: System.get_env("PORT") || 4001],
   server: false
 
@@ -10,7 +10,7 @@ config :blaces, BlacesWeb.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :blaces, Blaces.Repo,
+config :blaces, PinBin.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
