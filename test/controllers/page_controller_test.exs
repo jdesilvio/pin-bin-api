26424bb4f80292c5d44ba2523a266fbd9f946e9b
John DeSilvio <john.desilvio@yahoo.com>
2018-08-25 16:13:20 -0400

    Fix tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PinBinWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to PinBin!"
+    assert html_response(conn, 200) =~ "Welcome to Pin Bin!"
   end
 end
