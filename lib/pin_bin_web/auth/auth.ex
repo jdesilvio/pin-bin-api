3781507fe3fea2633529baf20e84c0bd917ab76a
John DeSilvio <john.desilvio@yahoo.com>
2018-12-01 13:11:34 -0500

    :scissors: Browser login/logout

--- lib/pin_bin_web/auth/auth.ex
+++ lib/pin_bin_web/auth/auth.ex
@@ -1,34 +1,11 @@
 defmodule PinBinWeb.Auth do
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
-  alias PinBin.User
   alias PinBin.Accounts
-
-  def login(conn, user) do
-    conn
-    |> Guardian.Plug.sign_in(user)
-  end
 
   def api_login(conn, user) do
     conn
     |> Guardian.Plug.api_sign_in(user)
-  end
-
-  def logout(conn) do
-    Guardian.Plug.sign_out(conn)
-  end
-
-  def login_by_email_and_pass(conn, email, password) do
-    user = Accounts.get_user_by_email(email)
-    cond do
-      user && checkpw(password, user.password_hash) ->
-        {:ok, login(conn, user)}
-      user ->
-        {:error, :unauthorized, conn}
-      true ->
-        dummy_checkpw
-        {:error, :not_found, conn}
-    end
   end
 
   def api_login_by_email_and_pass(conn, email, password) do
