35834e3f4dfe2e28a1bf7cff163b94fdfb61c501
John DeSilvio <john.desilvio@yahoo.com>
2018-12-01 13:53:26 -0500

    HTML is no longer needed

--- lib/pin_bin_web.ex
+++ lib/pin_bin_web.ex
@@ -47,9 +47,6 @@ defmodule PinBinWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
-
       import PinBinWeb.Router.Helpers
       import PinBinWeb.ErrorHelpers
       import PinBinWeb.Gettext
