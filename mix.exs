f3ee52bd4f12449cd23ffc0b14e709055512a636
John DeSilvio <john.desilvio@yahoo.com>
2018-12-01 14:28:55 -0500

    Bump Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PinBin.Mixfile do
   def project do
     [app: :pin_bin,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.7",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
